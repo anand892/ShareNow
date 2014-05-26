04055457f321814108ad7a2a6df1dfd2d5a6d0e5
AnandRaj Thangaraj <anand892@gmail.com>
2014-05-26 17:15:30 -0500

    Initial Foundation

---
+++ ShareNow/ShareNow/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  ShareNow
+//
+//  Created by AnandRaj Thangaraj on 5/26/14.
+//  Copyright (c) 2014 LiveDream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <Slt/Slt.h>
+#import <OpenEars/FliteController.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShareNow/ShareNow/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShareNow
+//
+//  Created by AnandRaj Thangaraj on 5/26/14.
+//  Copyright (c) 2014 LiveDream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
